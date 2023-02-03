71f04fbba35ff6386c9974bf59cc8726a7dc59f6
Levi Williams <ljw758@hotmail.com>
2023-02-02 20:33:04 -0600

    Completed Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string bakedGood = "Pie";
+            int bakeTime = 20;
+            char exclamation = '!';
+            bool isBakedGoodGlutenFree = false;
+            double coolOffTime = 10.5;
+            decimal timeToEat = 1.43m;
+
+            Console.WriteLine($"I will make {bakedGood} for dessert tonight. It should take {bakeTime} minutes to complete{exclamation}" +
+                $"After removing from the oven it will take {coolOffTime} minutes to cool. I am so hungry I am sure it will only take {timeToEat}" +
+                $" minutes to eat! ");
+
+            if (isBakedGoodGlutenFree == false)
+            {
+                Console.WriteLine("I should mention, however, this pie is not gluten free");
+            }
+            else
+                    {
+                Console.WriteLine("And to add to how tasty it is, it's even gluten free!");
+            }
+
+
         }
     }
 }
